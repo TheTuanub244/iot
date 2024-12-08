ede2631da4d401eed930035b1f6524dfe1d24e58
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 15:38:02 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const mqtt = require('mqtt');
 const { MongoClient } = require('mongodb');
+const connectDB = require('./connectMongoDB');
 
 const mongoUri = process.env.MONGO_URI || "mongodb+srv://vercel-admin-user:fUMbRXDPALiyi43s@cluster0.jin5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const client = new MongoClient(mongoUri, {
-    serverSelectionTimeoutMS: 30000  
-});
+connectDB()
 
 const mqttClient = mqtt.connect("mqtts://eb4d5208a7ea4b5ea2269b63abb4237c.s1.eu.hivemq.cloud:8883", {
     username: process.env.MQTT_USERNAME || 'a',
@@ -17,17 +16,7 @@ const mqttClient = mqtt.connect("mqtts://eb4d5208a7ea4b5ea2269b63abb4237c.s1.eu.
 const TOPICS = ["IoT/OutDoor", "IoT/InDoor"];
 
 async function setupMqttAndMongo() {
-    try {
-        console.log("Attempting to connect to MongoDB...");
-        await client.connect();
-        console.log("Connected to MongoDB");
-        const database = client.db("iot");
-        console.log("Database selected:", database.databaseName);
-
-        // Commented out MQTT-related code
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+    
 }
 
 setupMqttAndMongo().catch(console.error);
