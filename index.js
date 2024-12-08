b013034ee99411ca1b24e491f8bbd05a97be6ba5
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-08 15:18:49 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
+const express = require("express");
+
 const mqtt = require('mqtt');
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose')
 const mongoUri = process.env.MONGO_URI || "mongodb+srv://tuanub244:AtOePUkyLEbKvv16@it4409.0ybva.mongodb.net/?retryWrites=true&w=majority&appName=it4409";
 
-// const client = new MongoClient(mongoUri, {
-//     serverSelectionTimeoutMS: 10000, 
-//     socketTimeoutMS: 45000,
-// });
-
+const app = express()
+app.use(express.json());
+const connectDB = require("./connectMongoDB");
 const mqttClient = mqtt.connect("mqtts://eb4d5208a7ea4b5ea2269b63abb4237c.s1.eu.hivemq.cloud:8883", {
     username: process.env.MQTT_USERNAME || 'a',
     password: process.env.MQTT_PASSWORD || 'a',
@@ -20,25 +20,26 @@ const TOPICS = ["IoT/OutDoor", "IoT/InDoor"];
 async function setupMqttAndMongo() {
     try {
         console.log("Attempting to connect to MongoDB...");
-        const client = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://tuanub244:AtOePUkyLEbKvv16@it4409.0ybva.mongodb.net/?retryWrites=true&w=majority&appName=it4409', {
-            serverSelectionTimeoutMS: 7000,
-          }).then(() => {
-            console.log('Connected to MongoDB');
-          }).catch((error) => {
-            console.error('Error connecting to MongoDB:', error);
-          });
+        connectDB();
+        // const client = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://tuanub244:AtOePUkyLEbKvv16@it4409.0ybva.mongodb.net/?retryWrites=true&w=majority&appName=it4409', {
+        //     serverSelectionTimeoutMS: 7000,
+        //   }).then(() => {
+        //     console.log('Connected to MongoDB');
+        //   }).catch((error) => {
+        //     console.error('Error connecting to MongoDB:', error);
+        //   });
   
-        const database = client.db("iotData");
-        mqttClient.on('connect', () => {
-            console.log('Connected to HiveMQ');
-            mqttClient.subscribe(TOPICS, (err) => {
-                if (!err) {
-                    console.log(`Subscribed to topics: ${TOPICS.join(', ')}`);
-                } else {
-                    console.error("Subscription error:", err);
-                }
-            });
-        });
+        // const database = client.db("iotData");
+        // mqttClient.on('connect', () => {
+        //     console.log('Connected to HiveMQ');
+        //     mqttClient.subscribe(TOPICS, (err) => {
+        //         if (!err) {
+        //             console.log(`Subscribed to topics: ${TOPICS.join(', ')}`);
+        //         } else {
+        //             console.error("Subscription error:", err);
+        //         }
+        //     });
+        // });
 
         // mqttClient.on('message', async (topic, message) => {
         //     console.log(`Received message on topic ${topic}: ${message.toString()}`);
